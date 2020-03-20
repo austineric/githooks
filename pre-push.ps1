4747ed070b654657261af706c1b25a5ebd31b11c
Eric Austin <eaustin@gnresound.com>
2020-03-20 15:18:33 -0500

    Updated

--- pre-push.ps1
+++ pre-push.ps1
@@ -3,7 +3,7 @@
 ####################################
 # Author:       Eric Austin
 # Description:  Pre-push git hook
-# Notes:        Stops processing if there's an error rather than automatically handle fixes which can become convoluted
+# Notes:        Pushes the repo to a specified location then removes unnecessary items
 #               Pre-push hooks receive the following arguments:
 #                   $Remote=$Args[0]
 #                   $URL=$Args[1]
@@ -13,16 +13,15 @@
 $ErrorActionPreference="Stop"
 
 #script variables
-$TemporaryBranch=""
 $Destination=""
-$ItemsToPush=''         #comma-separated names of the files and/or folders that should be pushed to the prod repo, double-quoted if needed, ie '"Folder 1", "Folder 2"'
-$OriginalBranch=""      #instantiate empty
+$ItemsToKeep=''         #comma-separated names of the files and/or folders that should be pushed to the prod repo, double-quoted if needed, ie '"Folder 1", "Folder 2"'
+$CurrentBranch=""       #instantiate empty
 $RemoveItemsCommand=""  #instantiate empty
 
 Try {
 
-    #get original branch
-    $OriginalBranch=(((git branch) | Where-Object { $_ -match "\*" })  -replace "\* ", "")
+    #get current branch
+    $CurrentBranch=(((git branch) | Where-Object { $_ -match "\*" })  -replace "\* ", "")
 
     Write-Host "---------------"
     Write-Host ""
@@ -35,59 +34,24 @@ Try {
         Throw "Git status returned outstanding changes"
     }
 
-	#switch to new branch
-	Write-Host "Switching to temporary branch..."
-    git checkout -b $TemporaryBranch --quiet
+    #push the branch (use no-verify to prevent this hook from being called again)
+    Write-Host "Pushing to prod repo..."
+    git push $Destination $CurrentBranch --no-verify --force --quiet
     if ($LASTEXITCODE -ne 0)
     {
-        Throw "Checking out temporary branch failed"
+        Throw "Pushing to prod repo failed"
     }
 
-    #remove all files except .git, .gitignore, and the specified directory
-    Write-Host "Removing any files that shouldn't be sent to prod repo..."
-    $RemoveItemsCommand="Get-ChildItem -Exclude .git\*, .gitignore, $ItemsToPush | Remove-Item -Recurse -Force"
+    #change to the destination directory
+    Write-Host "Changing location to prod repo..."
+    Set-Location -Path $Destination
+
+    #remove unnecessary items (use Invoke-Expression to properly handle multiple items with spaces in their names)
+    Write-Host "Removing unnecessary items..."
+    $RemoveItemsCommand="Get-ChildItem -Exclude .git\*, .gitignore, $ItemsToKeep | Remove-Item -Recurse -Force"
     Invoke-Expression -Command $RemoveItemsCommand
 
-    #stage the changes for committing
-    Write-Host "Staging changes for commit..."
-    git add .
-    if ($LASTEXITCODE -ne 0)
-    {
-        Throw "Staging commit in temporary branch failed"
-    }
-
-    #commit the changes
-    Write-Host "Committing changes..."
-    git commit -m "Automatic branch creation" --quiet
-    if ($LASTEXITCODE -ne 0)
-    {
-        Throw "Commit to temporary branch failed"
-    }
-
-    #push the branch (use no-verify to prevent this hook from being called again)
-    Write-Host "Pushing temporary branch to prod repo..."
-    git push $Destination $TemporaryBranch --no-verify --force --quiet
-    if ($LASTEXITCODE -ne 0)
-    {
-        Throw "Pushing temporary branch to prod repo failed"
-    }
-
-    #switch back to original branch
-    Write-Host "Switching back to original branch..."
-    git checkout $OriginalBranch --quiet
-    if ($LASTEXITCODE -ne 0) 
-    {
-        Throw "Switching back to $OriginalBranch failed"
-    }
-
-    #delete temporary branch
-    Write-Host "Deleting temporary branch..."
-    git branch -D $TemporaryBranch --quiet
-    if ($LASTEXITCODE -ne 0) 
-    {
-        Throw "Deleting temporary branch failed"
-    }
-
+    #return success code of 0
     Return 0
 
 }
